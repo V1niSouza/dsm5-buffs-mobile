8a4e62d248e278437395ab525b5abf318fa3c3d4
V1niSouza <viniciussouzaramospv@gmail.com>
2025-08-26 21:20:08 -0300

    refactor: refatora design Home

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,61 +1,38 @@
-// HomeScreen.tsx
 import React, { useState } from "react";
-import { View, ScrollView, StyleSheet, Animated, Text } from "react-native";
+import { View, ScrollView, StyleSheet, TouchableOpacity } from "react-native";
 import Propriedades from "../components/Dropdown";
 import AlertasPendentes from "../components/Lembretes";
 import DashPropriedade from "../components/DashPropriedade";
 import { colors } from "../styles/colors";
 import BuffsLogo from '../../assets/images/logoBuffs.svg'; 
+import User from '../../assets/images/user.svg';
+import { MainLayout } from "../layouts/MainLayout";
 
 export const HomeScreen = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  const scrollY = React.useRef(new Animated.Value(0)).current;
-
-  const header2Opacity = scrollY.interpolate({
-    inputRange: [0, 50], // altura do scroll para desaparecer
-    outputRange: [1, 0],
-    extrapolate: 'clamp',
-  });
-
-  const header1TextOpacity = scrollY.interpolate({
-    inputRange: [0, 50],
-    outputRange: [0, 1],
-    extrapolate: 'clamp',
-  });
 
   return (
-    <View>
-      <View style={styles.headerNotifications}>
-        {/* Texto centralizado */}
-        <Animated.View 
-          style={[
-            { opacity: header1TextOpacity,  alignItems: 'center'}
-          ]}
-        >
-          <BuffsLogo width={80} height={80} />
-        </Animated.View>
+    <View style={styles.container}>
+      <View style={styles.header}>
+          {/* Texto centralizado */}
+          <View style={{alignItems: 'center'}}>
+            <BuffsLogo width={90} height={90} />
+          </View>
+          <View style={styles.headerButtons}>
+            <TouchableOpacity onPress={() => console.log("Tanque")} style={styles.button}>
+              <User width={20} height={20} style={{ margin: 6}}/>
+            </TouchableOpacity>
+          </View>
       </View>
-      {/* Conteúdo rolável */}
-      <Animated.ScrollView
-        onScroll={Animated.event(
-          [{ nativeEvent: { contentOffset: { y: scrollY } } }],
-          { useNativeDriver: true }
-        )}
-        scrollEventThrottle={16}
-        contentContainerStyle={{ paddingBottom: 25 }}
-      >
-        <Animated.View style={[styles.header2, { opacity: header2Opacity}]}>
-          <BuffsLogo width={90} height={90} style={{ marginBottom: 10 }} />
-        </Animated.View>
-    <ScrollView 
-      style={styles.container}
-      scrollEnabled={!dropdownOpen} // trava o scroll quando dropdown está aberto
-    >
-      <Propriedades dropdownOpen={dropdownOpen} setDropdownOpen={setDropdownOpen} />
-      <DashPropriedade />
-      <AlertasPendentes />
-    </ScrollView>
-      </Animated.ScrollView>
+      <MainLayout>
+        <ScrollView 
+          scrollEnabled={!dropdownOpen} // trava o scroll quando dropdown está aberto
+          >
+          <Propriedades dropdownOpen={dropdownOpen} setDropdownOpen={setDropdownOpen} />
+          <DashPropriedade />
+          <AlertasPendentes />
+        </ScrollView>
+      </MainLayout>
     </View>
   );
 };
@@ -63,28 +40,25 @@ export const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
+    backgroundColor: '#222'
   },
-    headerNotifications: {
-    height: 60,
+  header: {
+    height: 80,
     backgroundColor: colors.yellow.base,
     justifyContent: 'center',
     paddingLeft: 16,
     paddingTop: 20,
+    borderColor: colors.yellow.dark
   },
-  header1Text: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: colors.brown.base,
+  headerButtons: {
+    marginTop: 30,
+    flexDirection: "row",
+    position: "absolute",
+    right: 20, // fixa os botões à direita
+    gap: 20, // espaço entre eles
   },
-  header2: {
-    height: 35,
-    justifyContent: 'center',
-    paddingLeft: 16,
-    backgroundColor: colors.yellow.base,
-  },
-  header2Text: {
-    fontSize: 22,
-    fontWeight: 'bold',
+  button: {
+    backgroundColor: colors.yellow.dark,
+    borderRadius: 50,
   },
 });
